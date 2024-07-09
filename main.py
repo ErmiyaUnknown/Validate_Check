7bc099e5a228158a5c7c396032effe5b7414f0b5
Ermiya Askari <133347555+ErmiyaUnknown@users.noreply.github.com>
2024-07-09 13:35:12 +0330

    some bugs fixed for this code!

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-email = input("email: ")
-phone = input("phone: ")
-
 def email_validator(email):
     
-    if email.find("@") != -1:
+    if email.find("@") != -1 and email.find(".") != -1:
         email_username = email.split("@")
         
-        if email_username[0].isalnum() == True:
+        if email_username[0].isalnum() == True or (email_username[0].isalnum() == False and email_username[0].find("_") != -1):
             email_domain = email_username[1].split(".")
             
             if email_domain[0].isalnum() == True and email_domain[1].find("_") == -1:
@@ -23,7 +20,7 @@ def email_validator(email):
     else:    
         raise Exception(f"invalid email address: {email}")  
     
-email_validator(email)    
+    
 
 
 def phone_validator(phone): 
@@ -34,11 +31,26 @@ def phone_validator(phone):
     elif len(phone) == 13 and phone.startswith("+989") == True:
          print("phonenumber is true")
     
-    elif len(phone) == 14 and phone.startswith("+9899") == True:
+    elif len(phone) == 14 and phone.startswith("00989") == True:
         print("phonenumber is true")
                 
     else:
         raise Exception(f"invalid phone number: {phone}")   
      
      
-phone_validator(phone)     
+    
+while True:
+    
+    email = input("email: ")
+    if email == "exit":
+        break
+    
+    email_validator(email)
+    
+    
+    phone = input("phone: ")
+    if phone == "exit":
+        break
+    
+    phone_validator(phone) 
+    
